8a3dae1c3008b7a7bf7008c4e32ae679ac2eb435
Chrystian Remes <chrystian@windowslive.com>
2024-10-16 17:25:08 -0300

    Adding 'ticket' variable

--- main_lstm_forecast.py
+++ main_lstm_forecast.py
@@ -30,8 +30,9 @@ import numpy as np
 
 end_date = date.today().strftime("%Y-%m-%d")
 start_date = '2020-01-01'
+ticket = 'PETR4.SA'
 
-df = yf.download('PETR4.SA', start=start_date, end=end_date)
+df = yf.download(ticket, start=start_date, end=end_date)
 
 # Inspect the data
 print(df.head())
