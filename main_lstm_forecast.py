41ebb9b8a7af12659b0a53b36d11fdc3c6b4f0aa
Chrystian Remes <chrystian@windowslive.com>
2024-10-23 23:00:29 -0300

    Added mlflow Experiment Tracking and Group Comments

--- main_lstm_forecast.py
+++ main_lstm_forecast.py
@@ -1,6 +1,9 @@
 import mlflow
 import mlflow.pytorch
 
+mlflow.set_tracking_uri(uri="http://127.0.0.1:7777")
+mlflow.set_experiment("LSTM Experiments")
+
 ############################################################################
 ##################  SOME GENERAL PLOT  CONFIGURATIONS  #####################
 ############################################################################
@@ -28,11 +31,11 @@ from datetime import date
 import pandas as pd
 import numpy as np
 
-end_date = date.today().strftime("%Y-%m-%d") # GRUPO : DECIDIR
-start_date = '2020-01-01'                    # GRUPO : DECIDIR
+stock_end_date = date.today().strftime("%Y-%m-%d") # GRUPO : DECIDIR
+stock_start_date = '2020-01-01'                    # GRUPO : DECIDIR
 ticket = 'PETR4.SA'
 
-df = yf.download(ticket, start=start_date, end=end_date)
+df = yf.download(ticket, start=stock_start_date, end=stock_end_date)
 
 # Inspect the data
 print(df.head())
@@ -78,13 +81,13 @@ test_data = df[training_data_len:].iloc[:, :1]
 print(train_data.shape, test_data.shape)
 
 # Selecting Open Price values
-dataset_train = train_data.Open.values  # GRUPO : VERIFICAR
+dataset_train = train_data.Open.values  # GRUPO : CLOSE
 # Reshaping 1D to 2D array
 dataset_train = np.reshape(dataset_train, (-1, 1))
 print(dataset_train.shape)
 
 # Selecting Open Price values
-dataset_test = test_data.Open.values    # GRUPO : VERIFICAR
+dataset_test = test_data.Open.values    # GRUPO : CLOSE
 # Reshaping 1D to 2D array
 dataset_test = np.reshape(dataset_test, (-1, 1))
 print(dataset_test.shape)
@@ -96,11 +99,11 @@ print(dataset_test.shape)
 from sklearn.preprocessing import MinMaxScaler
 
 scaler = MinMaxScaler(feature_range=(0, 1))     # GRUPO : DECIDIR
-# Scaling dataset
+# Scaling dataset - FIT SÓ AQUI
 scaled_train = scaler.fit_transform(dataset_train)
 print(scaled_train[:5])
 
-# Normalizing values between 0 and 1
+# Normalizing values between 0 and 1 - AQUI SÓ TRANSFORM
 scaled_test = scaler.fit_transform(dataset_test)
 print(scaled_test[:5])
 
@@ -108,20 +111,22 @@ print(scaled_test[:5])
 ##############  SPLIT DATA INTO X (INPUTS) AND Y (LABLES)  #################
 ############################################################################
 
+# COLOCAR ESSE STEP NA PIPELINE DE DADOS
+
 # Create sequences and labels for training data
-sequence_length = 50  # Number of time steps to look back   / # GRUPO : VERIFICAR
+sequence_length_train = 50  # Number of time steps to look back   / # GRUPO : VERIFICAR
 X_train, y_train = [], []
-for i in range(len(scaled_train) - sequence_length):
-    X_train.append(scaled_train[i:i + sequence_length])
-    y_train.append(scaled_train[i + sequence_length])  # Predicting the value right after the sequence
+for i in range(len(scaled_train) - sequence_length_train):
+    X_train.append(scaled_train[i:i + sequence_length_train])
+    y_train.append(scaled_train[i + sequence_length_train])  # Predicting the value right after the sequence
 X_train, y_train = np.array(X_train), np.array(y_train)
 
 # Create sequences and labels for testing data
-sequence_length = 30  # Number of time steps to look back   / # GRUPO : VERIFICAR
+sequence_length_test = 30  # Number of time steps to look back   / # GRUPO : COLOCAR IGUAL
 X_test, y_test = [], []
-for i in range(len(scaled_test) - sequence_length):
-    X_test.append(scaled_test[i:i + sequence_length])
-    y_test.append(scaled_test[i + sequence_length])  # Predicting the value right after the sequence
+for i in range(len(scaled_test) - sequence_length_test):
+    X_test.append(scaled_test[i:i + sequence_length_test])
+    y_test.append(scaled_test[i + sequence_length_test])  # Predicting the value right after the sequence
 X_test, y_test = np.array(X_test), np.array(y_test)
 
 ############################################################################
@@ -149,6 +154,7 @@ print(X_test.shape, y_test.shape)
 ############################################################################
 
 class LSTMModel(nn.Module):
+    # AVALIAR INICIALIZAÇÃO DOS PESOS
     def __init__(self, input_size, hidden_size, num_layers, dropout=0.2):
         super(LSTMModel, self).__init__()
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, dropout=dropout)
@@ -167,8 +173,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 
 input_size = 1          # GRUPO : HYPERPARAMETROS
-num_layers = 4          # GRUPO : HYPERPARAMETROS
-hidden_size = 180       # GRUPO : HYPERPARAMETROS
+num_layers = 50          # GRUPO : HYPERPARAMETROS
+hidden_size = 50       # GRUPO : HYPERPARAMETROS
 output_size = 1
 dropout = 0.2           # Regulatization // GRUPO : HYPERPARAMETROS
 learning_rate = 0.0005  # GRUPO : HYPERPARAMETROS
@@ -177,7 +183,7 @@ model = LSTMModel(input_size, hidden_size, num_layers, dropout).to(device)
 loss_fn = nn.MSELoss(reduction='mean')  # GRUPO : DECIDIR
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)  # GRUPO : DECIDIR
 
-batch_size = 32  # GRUPO : VERIFICAR
+batch_size = 60  # GRUPO : VERIFICAR
 train_dataset = TensorDataset(X_train, y_train)
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_dataset = TensorDataset(X_test, y_test)
@@ -191,116 +197,149 @@ test_hist = []
 ###################  LSTM TRAINING // LSTM TRANING  ########################
 ############################################################################
 
+params_to_log = {
+    "start_date" : stock_start_date,
+    "end_date" : stock_end_date,
+    "batch_size" : batch_size,
+    "sequence_length_train" : sequence_length_train,
+    "sequence_length_test" : sequence_length_test,
+    "input_size" : input_size,
+    "num_layers" : num_layers,
+    "hidden_size" : hidden_size,
+    "dropout" : dropout,
+    "learning_rate" : learning_rate,
+    "num_epochs" : num_epochs,
+    "optimizer" : optimizer,
+    "loss_fn" : loss_fn,
+}
+
+
 import time
 
-start_time = time.time()
-print(f"Starting training: {start_time}")
+start_training_time = time.time()
+print(f"Starting training: {start_training_time}")
 
-for epoch in range(num_epochs):
-    total_loss = 0.0
-    model.train()
-    for batch_X, batch_y in train_loader:
-        batch_X, batch_y = batch_X.to(device), batch_y.to(device)
-        predictions = model(batch_X)
-        loss = loss_fn(predictions, batch_y)
+with mlflow.start_run():
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+    mlflow.set_tag("Fase","1 - Define Baseline")
+    mlflow.log_params(params_to_log)
 
-        total_loss += loss.item()
+    for epoch in range(num_epochs):
+        total_loss = 0.0
+        model.train()
+        for batch_X, batch_y in train_loader:
+            batch_X, batch_y = batch_X.to(device), batch_y.to(device)
+            predictions = model(batch_X)
+            loss = loss_fn(predictions, batch_y)
 
-    average_loss = total_loss / len(train_loader)
-    train_hist.append(average_loss)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
+            total_loss += loss.item()
+
+        average_loss = total_loss / len(train_loader)
+        train_hist.append(average_loss)
+
+        model.eval()
+        with torch.no_grad():
+            total_test_loss = 0.0
+
+            for batch_X_test, batch_y_test in test_loader:
+                batch_X_test, batch_y_test = batch_X_test.to(device), batch_y_test.to(device)
+                predictions_test = model(batch_X_test)
+                test_loss = loss_fn(predictions_test, batch_y_test)
+
+                total_test_loss += test_loss.item()
+
+            average_test_loss = total_test_loss / len(test_loader)
+            test_hist.append(average_test_loss)
+
+        if (epoch + 1) % 10 == 0:
+            print(f'Epoch [{epoch + 1}/{num_epochs}] - Training Loss: {average_loss:.4f}, Test Loss: {average_test_loss:.4f}')
+
+    end_training_time = time.time()
+    elapsed_training_time = end_training_time - start_training_time
+    print(f"Ending training: {end_training_time}")
+    print(f"Elapsed Training time: {elapsed_training_time}")
+    
+    mlflow.log_metric('training_time', elapsed_training_time)
+    mlflow.log_metric('training_loss', average_loss)
+    mlflow.log_metric('test_loss', average_test_loss)
+    
+
+    ############################################################################
+    #########################  PLOT TRAINING RESULTS  ##########################
+    ############################################################################
+
+    x = np.linspace(1,num_epochs,num_epochs)
+    plt.plot(x,train_hist,scalex=True, label="Training loss")
+    plt.plot(x, test_hist, label="Test loss")
+    plt.legend()
+    plt.show()
+
+    ############################################################################
+    #################### PREDICT // FORECAST RESULTS  ##########################
+    ############################################################################
+
+    num_forecast_steps = 30
+    sequence_to_plot = X_test.squeeze().cpu().numpy()
+    historical_data = sequence_to_plot[-1]
+
+    forecasted_values = []
+    with torch.no_grad():
+        for _ in range(num_forecast_steps):
+            historical_data_tensor = torch.as_tensor(historical_data).view(1, -1, 1).float().to(device)
+            predicted_value = model(historical_data_tensor).cpu().numpy()[0, 0]
+            forecasted_values.append(predicted_value)
+            historical_data = np.roll(historical_data, shift=-1)
+            historical_data[-1] = predicted_value
+
+    last_date = test_data.index[-1]
+    future_dates = pd.date_range(start=last_date + pd.DateOffset(1), periods=30)
+
+    ############################################################################
+    ####################### PLOT PREDICT // FORECAST ###########################
+    ############################################################################
+
+    from pylab import rcParams
+
+    plt.rcParams['figure.figsize'] = [14, 4]
+    plt.plot(test_data.index[-100:], test_data.Open[-100:], label="test_data", color="b")
+    plt.plot(test_data.index[-30:], test_data.Open[-30:], label='actual values', color='green')
+    plt.plot(test_data.index[-1:].append(future_dates), np.concatenate([test_data.Open[-1:], scaler.inverse_transform(np.array(forecasted_values).reshape(-1, 1)).flatten()]), label='forecasted values', color='red')
+
+    plt.xlabel('Time Step')
+    plt.ylabel('Value')
+    plt.legend()
+    plt.title('Time Series Forecasting')
+    plt.grid(True)
+    plt.show()
+
+    ############################################################################
+    #######################   PERFORMANCE METRICS   ###########################
+    ############################################################################
+
+    from sklearn.metrics import root_mean_squared_error, r2_score
+
+    # Evaluate the model and calculate RMSE and R² score
     model.eval()
     with torch.no_grad():
-        total_test_loss = 0.0
+        test_predictions = []
+        for batch_X_test in X_test:
+            batch_X_test = batch_X_test.to(device).unsqueeze(0)  # Add batch dimension
+            test_predictions.append(model(batch_X_test).cpu().numpy().flatten()[0])
 
-        for batch_X_test, batch_y_test in test_loader:
-            batch_X_test, batch_y_test = batch_X_test.to(device), batch_y_test.to(device)
-            predictions_test = model(batch_X_test)
-            test_loss = loss_fn(predictions_test, batch_y_test)
+    test_predictions = np.array(test_predictions)
 
-            total_test_loss += test_loss.item()
+    # Calculate RMSE and R² score
+    rmse = root_mean_squared_error(y_test.cpu().numpy(), test_predictions)
+    r2 = r2_score(y_test.cpu().numpy(), test_predictions)
 
-        average_test_loss = total_test_loss / len(test_loader)
-        test_hist.append(average_test_loss)
+    print(f'RMSE: {rmse:.4f}')
+    print(f'R² Score: {r2:.4f}')
 
-    if (epoch + 1) % 10 == 0:
-        print(f'Epoch [{epoch + 1}/{num_epochs}] - Training Loss: {average_loss:.4f}, Test Loss: {average_test_loss:.4f}')
+    mlflow.log_metric("RMSE", rmse)
+    mlflow.log_metric("R2-Score", r2)
 
-end_time = time.time()
-print(f"Ending training: {end_time}")
-print(f"Elapsed Training time: {end_time - start_time}")
-
-############################################################################
-#########################  PLOT TRAINING RESULTS  ##########################
-############################################################################
-
-x = np.linspace(1,num_epochs,num_epochs)
-plt.plot(x,train_hist,scalex=True, label="Training loss")
-plt.plot(x, test_hist, label="Test loss")
-plt.legend()
-plt.show()
-
-############################################################################
-#################### PREDICT // FORECAST RESULTS  ##########################
-############################################################################
-
-num_forecast_steps = 30
-sequence_to_plot = X_test.squeeze().cpu().numpy()
-historical_data = sequence_to_plot[-1]
-
-forecasted_values = []
-with torch.no_grad():
-    for _ in range(num_forecast_steps):
-        historical_data_tensor = torch.as_tensor(historical_data).view(1, -1, 1).float().to(device)
-        predicted_value = model(historical_data_tensor).cpu().numpy()[0, 0]
-        forecasted_values.append(predicted_value)
-        historical_data = np.roll(historical_data, shift=-1)
-        historical_data[-1] = predicted_value
-
-last_date = test_data.index[-1]
-future_dates = pd.date_range(start=last_date + pd.DateOffset(1), periods=30)
-
-############################################################################
-####################### PLOT PREDICT // FORECAST ###########################
-############################################################################
-
-from pylab import rcParams
-
-plt.rcParams['figure.figsize'] = [14, 4]
-plt.plot(test_data.index[-100:], test_data.Open[-100:], label="test_data", color="b")
-plt.plot(test_data.index[-30:], test_data.Open[-30:], label='actual values', color='green')
-plt.plot(test_data.index[-1:].append(future_dates), np.concatenate([test_data.Open[-1:], scaler.inverse_transform(np.array(forecasted_values).reshape(-1, 1)).flatten()]), label='forecasted values', color='red')
-
-plt.xlabel('Time Step')
-plt.ylabel('Value')
-plt.legend()
-plt.title('Time Series Forecasting')
-plt.grid(True)
-plt.show()
-
-############################################################################
-#######################   PERFORMANCE METRICS   ###########################
-############################################################################
-
-from sklearn.metrics import root_mean_squared_error, r2_score
-
-# Evaluate the model and calculate RMSE and R² score
-model.eval()
-with torch.no_grad():
-    test_predictions = []
-    for batch_X_test in X_test:
-        batch_X_test = batch_X_test.to(device).unsqueeze(0)  # Add batch dimension
-        test_predictions.append(model(batch_X_test).cpu().numpy().flatten()[0])
-
-test_predictions = np.array(test_predictions)
-
-# Calculate RMSE and R² score
-rmse = root_mean_squared_error(y_test.cpu().numpy(), test_predictions)
-r2 = r2_score(y_test.cpu().numpy(), test_predictions)
-
-print(f'RMSE: {rmse:.4f}')
-print(f'R² Score: {r2:.4f}')
+    mlflow.end_run()
